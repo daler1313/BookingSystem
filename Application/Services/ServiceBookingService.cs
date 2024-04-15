6130b3c98c15a63e563d223efc8bb80837ba35f2
Nilufar Mansuri <84025457+nilufarmansuri@users.noreply.github.com>
2024-04-15 09:40:36 +0500

    Added changes to application

---
+++ Application/Services/ServiceBookingService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ServiceBookingService : IBaseService<ServiceBooking>
+    {
+        private readonly IBaseRepository<ServiceBooking> _buildingRepository;
+
+        public ServiceBookingService(IBaseRepository<ServiceBooking> buildingRepository)
+        {
+            _buildingRepository = buildingRepository;
+        }
+
+        public async Task<ServiceBooking> CreateAsync(ServiceBooking building, CancellationToken token = default)
+        {
+            return await _buildingRepository.CreateAsync(building, token);
+        }
+
+        public async Task<bool> DeleteAsync(ServiceBooking building, CancellationToken token = default)
+        {
+            return await _buildingRepository.DeleteAsync(building, token);
+        }
+
+        public async Task<IEnumerable<ServiceBooking>> GetAllAsync(CancellationToken token = default)
+        {
+            return await _buildingRepository.GetAllAsync(token);
+        }
+
+        public async Task<ServiceBooking> GetAsync(Guid id, CancellationToken token = default)
+        {
+            return await _buildingRepository.GetAsync(id, token);
+        }
+
+        public async Task<bool> UpdateAsync(ServiceBooking building, CancellationToken token = default)
+        {
+            var existingBuilding = await GetAsync(building.Id);
+
+            if (existingBuilding is null)
+            {
+                return false;
+            }
+
+            existingBuilding.Name = building.Name;
+            existingBuilding.Address = building.Address;
+            existingBuilding.CeilingHeight = building.CeilingHeight;
+            existingBuilding.BuildingClass = building.BuildingClass;
+            existingBuilding.EntrancesCount = building.EntrancesCount;
+            existingBuilding.Material = building.Material;
+
+            return await _buildingRepository.UpdateAsync(existingBuilding, token);
+        }
+    }
+}
