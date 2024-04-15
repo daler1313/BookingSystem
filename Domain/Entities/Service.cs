a351aaa08a9838b57234081e99d71c022c5b8ce6
husejnm <t3528465@gmail.com>
2024-04-15 09:36:45 +0500

    eqeq

---
+++ Domain/Entities/Service.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Service: BaseEntity
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public Status Status { get; set; }
+        public virtual ICollection<ServiceDetails> ServiceDetails { get; set; }
+
+
+    }
+}
