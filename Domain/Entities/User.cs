551cae2de2a2f605fd527743734c9cf823402d59
husejnm <t3528465@gmail.com>
2024-04-15 09:57:21 +0500

    corrected user

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -8,7 +8,6 @@ namespace Domain.Entity
 {
     public class User: BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string UserName { get; set; }
         public string PhoneNumber { get; set; }
